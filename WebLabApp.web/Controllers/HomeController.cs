992782800fddf469579582bdd4b12685dc9c3569
Den <dmazurik4@gmail.com>
2023-12-19 12:37:03 +0200

    LAB 5-8

--- WebLabApp.web/Controllers/HomeController.cs
+++ WebLabApp.web/Controllers/HomeController.cs
@@ -2,6 +2,12 @@
 using WebLabApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+using System.Resources;
+using WebLabApp.web.Models;
 
 namespace WebLabApp.Web.Controllers
 {
@@ -9,15 +15,22 @@ namespace WebLabApp.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 		private readonly ISender _emailSender;
-		public HomeController(ILogger<HomeController> logger, ISender emailSender)
+        private readonly IStringLocalizer<HomeController> _localizer;
+        public HomeController(ILogger<HomeController> logger, ISender emailSender, IStringLocalizer<HomeController> localizer)
 		{
 			_logger = logger;
 			_emailSender=emailSender;
-		}
+            _localizer = localizer;
+        }
         public IActionResult Index()
         {
-            _logger.LogInformation("This is an information log from HomeController.");
-            return View();
+            var model = new LanguageSelectionModel
+            {
+                AvailableCultures = new[] { "en-US", "uk-UA" }, // Add other cultures if needed
+                CurrentCulture = System.Globalization.CultureInfo.CurrentCulture.Name
+            };
+
+            return View(model);
         }
         [HttpPost]
 		public async Task<IActionResult> SendEmail([FromBody] EmailModel vm)
@@ -36,9 +49,24 @@ namespace WebLabApp.Web.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult LanguagePartial()
+        {
+            var model = new List<string> { "en", "fr", "uk" };
+            return PartialView("_LanguageSwitcher", model);
         }
     }
 }
--- WebLabApp.web/Program.cs
+++ WebLabApp.web/Program.cs
@@ -1,5 +1,9 @@
+using FluentValidation.AspNetCore;
 using WebAppLab.Services.EmailServices;
+using WebLabApp.web.Validations;
 using Serilog;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +15,33 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Host.UseSerilog();
+builder.Services.AddControllersWithViews()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<SampleModelValidator>())
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("uk-UA")
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<ISender, Sender>();
 var app = builder.Build();
+app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -31,6 +57,12 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "setLanguage",
+    pattern: "{culture}/{controller=Home}/{action=Index}",
+    defaults: new { controller = "Home", action = "Index" }
+);
 
 app.MapControllerRoute(
     name: "default",
